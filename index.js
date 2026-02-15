8c5f4deadc5bfb7f4e055a2cd1e72cb2b2f9169e
Dante Ro <or.etnad8@gmail.com>
2026-02-15 13:29:45 -0300

    eliminacion de fechas en servicios

--- index.js
+++ index.js
@@ -262,10 +262,6 @@ document.addEventListener('DOMContentLoaded', () => {
       el: '.swiper-pagination',
       clickable: true,
     },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
     breakpoints: {
       640: { slidesPerView: 2 },
       1024: { slidesPerView: 3 }
